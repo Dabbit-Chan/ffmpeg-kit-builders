95c1398a1c5f3504ca3f37be76053825e3c5ef20
akashskypatel <akashskypatel@gmail.com>
2025-12-26 06:05:05 +0000

    bundle preset build scripts

---
+++ scripts/builds/linux-64-video-ai-gpu-hw.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+small=""
+
+case "${2,,}" in
+  s|small|m|min)
+    small="--enable-small"
+    ;;
+  *);;
+esac
+cd "$(pwd)/../../" || exit 1
+case "$1" in
+  1)
+  ./runner.sh --host=linux --arch=x86_64 -y --video-ai-gpu-hw-bundle --build-deps-only $small
+  exit 0
+  ;;
+  2)
+  ./runner.sh --host=linux --arch=x86_64 -y --video-ai-gpu-hw-bundle --build-ffmpeg-only -f $small
+  exit 0
+  ;;
+  3)
+  ./runner.sh --host=linux --arch=x86_64 -y --video-ai-gpu-hw-bundle --build-ffmpeg-kit-only -f --release $small
+  exit 0
+  ;;
+  *)
+  ./runner.sh --host=linux --arch=x86_64 -y --video-ai-gpu-hw-bundle $small
+  exit 0
+  ;;
+esac
