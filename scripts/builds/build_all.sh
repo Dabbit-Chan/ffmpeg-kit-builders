4ef54391bd3e559f124f818662f3a1c4e09bde74
akashskypatel <akashskypatel@gmail.com>
2026-02-02 01:46:32 +0000

    add: preset build scripts

---
+++ scripts/builds/build_all.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -e
+# sudo ./linux-64-full.sh 1 gfy
+
+# sudo ./scripts/builds/linux-64-full.sh 2 gfy
+# sudo ./scripts/builds/linux-64-full.sh 3 gfy
+# sudo ./scripts/builds/linux-64-full.sh 2 fy
+# sudo ./scripts/builds/linux-64-full.sh 3 fy
+sudo ./scripts/builds/linux-64-full.sh 2 sgfy
+sudo ./scripts/builds/linux-64-full.sh 3 sgfy
+sudo ./scripts/builds/linux-64-full.sh 2 sfy
+sudo ./scripts/builds/linux-64-full.sh 3 sfy
+# sudo ./scripts/builds/linux-64-audio.sh 2 gfy
+# sudo ./scripts/builds/linux-64-audio.sh 3 gfy
+# sudo ./scripts/builds/linux-64-audio.sh 2 fy
+# sudo ./scripts/builds/linux-64-audio.sh 3 fy
+sudo ./scripts/builds/linux-64-audio.sh 2 sgfy
+sudo ./scripts/builds/linux-64-audio.sh 3 sgfy
+sudo ./scripts/builds/linux-64-audio.sh 2 sfy
+sudo ./scripts/builds/linux-64-audio.sh 3 sfy
+# sudo ./scripts/builds/linux-64-video.sh 2 gfy
+# sudo ./scripts/builds/linux-64-video.sh 3 gfy
+# sudo ./scripts/builds/linux-64-video.sh 2 fy
+# sudo ./scripts/builds/linux-64-video.sh 3 fy
+sudo ./scripts/builds/linux-64-video.sh 2 sgfy
+sudo ./scripts/builds/linux-64-video.sh 3 sgfy
+sudo ./scripts/builds/linux-64-video.sh 2 sfy
+sudo ./scripts/builds/linux-64-video.sh 3 sfy
+# sudo ./scripts/builds/linux-64-streaming.sh 2 gfy
+# sudo ./scripts/builds/linux-64-streaming.sh 3 gfy
+# sudo ./scripts/builds/linux-64-streaming.sh 2 fy
+# sudo ./scripts/builds/linux-64-streaming.sh 3 fy
+sudo ./scripts/builds/linux-64-streaming.sh 2 sgfy
+sudo ./scripts/builds/linux-64-streaming.sh 3 sgfy
+sudo ./scripts/builds/linux-64-streaming.sh 2 sfy
+sudo ./scripts/builds/linux-64-streaming.sh 3 sfy
+# sudo ./scripts/builds/linux-64-video-hw.sh 2 gfy
+# sudo ./scripts/builds/linux-64-video-hw.sh 3 gfy
+# sudo ./scripts/builds/linux-64-video-hw.sh 2 fy
+# sudo ./scripts/builds/linux-64-video-hw.sh 3 fy
+sudo ./scripts/builds/linux-64-video-hw.sh 2 sgfy
+sudo ./scripts/builds/linux-64-video-hw.sh 3 sgfy
+sudo ./scripts/builds/linux-64-video-hw.sh 2 sfy
+sudo ./scripts/builds/linux-64-video-hw.sh 3 sfy
