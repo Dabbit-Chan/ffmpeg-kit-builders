13d1c2cbad6168ea8ba9b3fdbe9f799f409b0b71
akashskypatel <akashskypatel@gmail.com>
2026-02-14 11:49:44 -0800

    feat: Introduce 'd' argument for conditional dependency build and activate additional build configurations.

--- scripts/builds/build_all.sh
+++ scripts/builds/build_all.sh
@@ -3,25 +3,31 @@ set -e
 
 # shellcheck disable=SC2317,SC2129,SC1091,SC2120,SC2035,SC2016,SC2310,SC2155,SC2154,SC2034
 p=""
+deps=""
 for arg; do
   case "${arg}" in
     linux|windows) 
       p="${arg:0:1}"
       break
       shift;;
-    *)
+    d) 
+      deps="y"
+      shift;;
+    *)  
     echo "Invalid platform"
     exit 1;
   esac
 done
 
-# sudo ./scripts/builds/64-full.sh 1 gfy${p}
+if [[ -n "$deps" ]]; then
+  sudo ./scripts/builds/64-full.sh 1 gfy${p}
+fi
 
-# sudo ./scripts/builds/64-base.sh 23 gfy${p}
-# sudo ./scripts/builds/64-base.sh 23 fy${p}
-# sudo ./scripts/builds/64-base.sh 23 sgfy${p}
-# sudo ./scripts/builds/64-base.sh 23 sfy${p}
-# sudo ./scripts/builds/64-full.sh 23 gfy${p}
+sudo ./scripts/builds/64-base.sh 23 gfy${p}
+sudo ./scripts/builds/64-base.sh 23 fy${p}
+sudo ./scripts/builds/64-base.sh 23 sgfy${p}
+sudo ./scripts/builds/64-base.sh 23 sfy${p}
+sudo ./scripts/builds/64-full.sh 23 gfy${p}
 sudo ./scripts/builds/64-full.sh 23 fy${p}
 sudo ./scripts/builds/64-full.sh 23 sgfy${p}
 sudo ./scripts/builds/64-full.sh 23 sfy${p}
