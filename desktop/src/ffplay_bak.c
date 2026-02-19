5a0db4ac36f3ba5c650fcf07e1e11364f16f564d
akashskypatel <akashskypatel@gmail.com>
2026-02-19 15:45:49 -0800

    fix: Corrected `audio_callback_time` to be a member of the `VideoState` struct.

--- desktop/src/ffplay_bak.c
+++ desktop/src/ffplay_bak.c
@@ -2461,7 +2461,7 @@ static int audio_decode_frame(VideoState *is)
     do {
 #if defined(_WIN32)
         while (frame_queue_nb_remaining(&is->sampq) == 0) {
-            if ((av_gettime_relative() - audio_callback_time) > 1000000LL * is->audio_hw_buf_size / is->audio_tgt.bytes_per_sec / 2)
+            if ((av_gettime_relative() - is->audio_callback_time) > 1000000LL * is->audio_hw_buf_size / is->audio_tgt.bytes_per_sec / 2)
                 return -1;
             av_usleep (1000);
         }
