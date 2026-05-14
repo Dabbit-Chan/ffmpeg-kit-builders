a5d8803ebaed17597a93fb8d051be18c626d6b11
akashslypatel <akashskypatel@gmail.com>
2026-05-14 16:34:00 -0700

    fix: separate maven and github publishing

--- scripts/android/build_aar.sh
+++ scripts/android/build_aar.sh
@@ -516,7 +516,8 @@ for key in "${!PLATFORMS[@]}"; do
               echo "${FFMPEG_KIT_JNI_LIBS_DIR}" > >(redirect_output)
               if find "${FFMPEG_KIT_JNI_LIBS_DIR}" -type f \( -name "*.so" -o -name "*.a" \) | read -r; then
                 [[ "${create_aar}" == "true" ]] && create_aar_artifact "${FFMPEG_KIT_JNI_LIBS_DIR}" "${FFMPEG_KIT_OUTPUT_NAME}" || true
-                [[ "${create_release}" == "true" ]] && release_asset=$(realpath "${BASEDIR}/tools/android/build/outputs/aar/${FFMPEG_KIT_OUTPUT_NAME}-${assemble_type,,}.aar") && create_release_artifact "${release_asset}" || true
+                [[ "${create_release}" == "true" ]] && release_asset=$(realpath "${BASEDIR}/tools/android/build/outputs/aar/${FFMPEG_KIT_OUTPUT_NAME}-${assemble_type,,}.aar") || true
+                [[ "${create_release}" == "true" && "$local_build" != "true" ]] && create_release_artifact "${release_asset}" || true
               fi
           done
       done
